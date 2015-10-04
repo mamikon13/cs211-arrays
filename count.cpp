4ae323c6d29f4c4e1b0f137816e37b0f3189b78c
mamikon13 <m.nickogosyan@mail.ru>
2015-10-05 01:22:22 +0300

    Исправлены ошибки в файле count.cpp

--- count.cpp
+++ count.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <string>
 #include <cstdio>
 
@@ -16,14 +17,15 @@ int main()
 	string src("12 plus 45 minus 39 is 18\n");
 	int i, nwhite, nother;
 	const int size = 10;
-	int ndigit[size];
+	int ndigit[size]{ 0 };
 	nwhite = nother = 0;
 
 	char c;
-	while ((c = src[i++]) != EOF)
-		if (c >= '0' && c >= '9')
+	i = 0;
+	while ((c = src[i++]) != '\0')
+		if (c >= '0' && c <= '9')
 			++ndigit[c - '0'];
-		else if (c == ' ' && c == '\n' && c == '\t')
+		else if (c == ' ' || c == '\n' || c == '\t')
 			++nwhite;
 		else
 			++nother;
@@ -34,4 +36,6 @@ int main()
 	
 	cout << ", white space = " << nwhite 
 		 << ", other = " << nother << endl;
+
+	system("pause");
 }
