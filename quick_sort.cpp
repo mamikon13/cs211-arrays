a2b0f0ecc252f119ccb4e67fad53ba60f97ebbd6
mamikon13 <m.nickogosyan@mail.ru>
2015-10-05 02:43:50 +0300

    Исправлен файл quick_sort.cpp и в нем добавлены тесты

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
- 
+#include <cassert>
 #include <cstdlib>
  
 using std::rand;
@@ -13,7 +13,7 @@ using std::swap;
 int * partition(int * left, int * right, int pivot) {
 	int * store = left; // место для вставки элементов, меньших pivot
 	for (int * p = left; p != right; ++p)
-		if (*p < pivot)
+		if (*p <= pivot)
 			swap(*p, *store++);
 	return store;
 }
@@ -21,8 +21,28 @@ int * partition(int * left, int * right, int pivot) {
 void my_qsort(int * arr, int n) {
 	if (n <= 1)
 		return; // массив в 1 или 0 элементов уже упорядочен
-	int * pivotPtr = arr + rand() % n; // случайный выбор опорного элемента
-	int newPivotIdx = partition(arr, arr + n, *pivotPtr) - arr;
-	my_qsort(arr, newPivotIdx + 1);
-	my_qsort(arr + newPivotIdx, n - (newPivotIdx + 1));
+	int pivotPtr = *(arr + rand() % n); // случайный выбор опорного элемента
+	int newPivotIdx = partition(arr, arr + n, pivotPtr) - arr;
+	my_qsort(arr, newPivotIdx);
+	my_qsort(arr + newPivotIdx, n - newPivotIdx);
+}
+
+template <typename T>
+bool Arrays_Are_Equal(const T*arr1, const T*arr2, int size)
+{
+	for (int j = 0; j < size; j++)
+		if (arr1[j] != arr2[j])
+			return false;
+	return true;
+}
+
+int main()
+{
+	int arr[]{ 1,3,2,-53,23,0,-4 };
+	int sortArr[]{ -53,-4,0,1,2,3,23 };
+	my_qsort(arr, 7);
+	assert(Arrays_Are_Equal(arr, sortArr, 7));
+
+	std::cout << "Tests successfully!" << std::endl;
+	system("pause");
 }
